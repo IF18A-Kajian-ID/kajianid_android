96bb664de7e0f2de4e7b54d94785b111042c21fe
fendi-riawan <53111473+fendi-riawan@users.noreply.github.com>
2020-12-11 16:33:39 +0700

    18.11.0268 Fendi Riawan (Membuat Tampilan Kajian)
    Membuat Tampilan Kajian dengan Membuat :
    1. Membuat KajianActivity.java dan Mengisi activity_kajian.xml
    2. Membuat ShowKajianActivity dan Mengisi activity_show_kajian.xml
    3. Membuat dan Mengisi content_show_kajian.xml
    4. Membuat dan Mengisi list_kajian.xml

---
+++ user/src/main/java/com/kajianid/android/ShowKajianActivity.java
@@ -0,0 +1,14 @@
+package com.kajianid.android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShowKajianActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_kajian);
+    }
+}
