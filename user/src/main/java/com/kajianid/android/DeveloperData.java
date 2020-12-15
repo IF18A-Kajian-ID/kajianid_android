6e7f0c61d0db5a8b95ac1a7513e44133885b7e7a
dwichan0905 <52048022+dwichan0905@users.noreply.github.com>
2020-12-15 22:06:21 +0700

    18.11.0004: Class dan Resource untuk Data Diri
    CATATAN:
    Silakan isi data diri kalian di
    User: com.kajianid.android/DeveloperData.java
    Ustadz: com.kajianid.ustadz/utils/DeveloperData.java

---
+++ user/src/main/java/com/kajianid/android/DeveloperData.java
@@ -0,0 +1,71 @@
+package com.kajianid.android;
+
+import androidx.annotation.DrawableRes;
+
+public class DeveloperData {
+    @DrawableRes
+    public static final int[] drawableId = {
+            R.drawable.dwichan,
+            R.drawable.maratun,
+            R.drawable.fendi,
+            R.drawable.intro1, // Diyah Primasari
+            R.drawable.intro3 // Muhammad Rizky Bayu Pratama
+    };
+
+    public static final String[] name = {
+            "Dwi Candra Permana",
+            "",
+            "",
+            "",
+            ""
+    };
+
+    public static final String[] nim = {
+            "18.11.0004",
+            "",
+            "",
+            "",
+            ""
+    };
+
+    public static final String[] kelas = {
+            "IF 2018 A",
+            "",
+            "",
+            "",
+            ""
+    };
+
+    public static final String[] yearRegister = {
+            "2018",
+            "",
+            "",
+            "",
+            ""
+    };
+
+    public static final String[] githubUsername = {
+            "dwichan0905",
+            "",
+            "",
+            "",
+            ""
+    };
+
+    public static final String[] email = {
+            "dwichan@outlook.com",
+            "",
+            "",
+            "",
+            ""
+    };
+
+    public static final String[] address = {
+            "Jalan Mandor Martinem Roy 1 Nomor 99 RT 005 RW 006 Desa Rejasari Kecamatan Langensari Kota Banjar",
+            "",
+            "",
+            "",
+            ""
+    };
+    
+}
