a71daf1660c0785919a9694332ae3d09702d413b
maratun1725 <52594711+maratun1725@users.noreply.github.com>
2020-12-17 16:33:24 +0700

    Pembuatan layout artikel, string, drawable, dan directory package adapter
    1.  Pembuatan beberapa Layout Article dengan nama :
    - activity_article_islami.xml
    - activity_content_read_article.xml
    - activity_list_article.xml
    -  activity_read_article.xml
    2. Penambahan String dan drawable untuk menunjang layout article
    3. Pembuatan package Adapter

---
+++ user/src/main/java/com/kajianid/android/ContentReadArticle.java
@@ -0,0 +1,14 @@
+package com.kajianid.android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ContentReadArticle extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_content_read_article);
+    }
+}
