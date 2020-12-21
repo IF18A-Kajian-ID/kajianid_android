c71bf868544192b38f037270baff1f51e2babdfe
dwichan0905 <52048022+dwichan0905@users.noreply.github.com>
2020-12-21 12:59:05 +0700

    Kelupaan 1 wkkwk

--- user/src/main/java/com/kajianid/android/data/Mosque.java
+++ user/src/main/java/com/kajianid/android/data/Mosque.java
@@ -1,4 +1,76 @@
 package com.kajianid.android.data;
 
-public class Mosque {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Mosque implements Parcelable {
+    private int id;
+    private String mosqueName;
+    private String latLng;
+
+    protected Mosque(Parcel in) {
+        id = in.readInt();
+        mosqueName = in.readString();
+        latLng = in.readString();
+        address = in.readString();
+    }
+
+    public static final Creator<Mosque> CREATOR = new Creator<Mosque>() {
+        @Override
+        public Mosque createFromParcel(Parcel in) {
+            return new Mosque(in);
+        }
+
+        @Override
+        public Mosque[] newArray(int size) {
+            return new Mosque[size];
+        }
+    };
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMosqueName() {
+        return mosqueName;
+    }
+
+    public void setMosqueName(String mosqueName) {
+        this.mosqueName = mosqueName;
+    }
+
+    public String getLatLng() {
+        return latLng;
+    }
+
+    public void setLatLng(String latLng) {
+        this.latLng = latLng;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    private String address;
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(id);
+        dest.writeString(mosqueName);
+        dest.writeString(latLng);
+        dest.writeString(address);
+    }
 }
