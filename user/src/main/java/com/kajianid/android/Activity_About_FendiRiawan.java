ba8f146d2f384ff5bf0fb30c358f27386576a3c3
fendi-riawan <53111473+fendi-riawan@users.noreply.github.com>
2020-12-22 13:46:10 +0700

    18.11.0268 Fendi Riawan Penugasan Pertemuan 10
    Fendi Riawan
    18.11.0268
    Tugas : Membuat Fitur Kajian

---
+++ user/src/main/java/com/kajianid/android/Activity_About_FendiRiawan.java
@@ -0,0 +1,14 @@
+package com.kajianid.android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_About_FendiRiawan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__about__fendi_riawan);
+    }
+}
