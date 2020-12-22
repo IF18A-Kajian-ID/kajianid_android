a7d0b49288953cf11aeabd4102b009f235ad632d
maratun1725 <52594711+maratun1725@users.noreply.github.com>
2020-12-22 13:42:52 +0700

    Membuat ActivityAboutMaratunSolehah

---
+++ user/src/main/java/com/kajianid/android/ActivityAboutMaratunSolehah.java
@@ -0,0 +1,14 @@
+package com.kajianid.android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityAboutMaratunSolehah extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_maratun_solehah);
+    }
+}
