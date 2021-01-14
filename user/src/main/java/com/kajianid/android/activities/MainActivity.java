a4a49350d39f5fe0c2bec672f1beb58ba1e0fdfe
dwichan0905 <52048022+dwichan0905@users.noreply.github.com>
2021-01-14 17:03:23 +0700

    18.11.0004: Main Activity. Main of the application!

--- user/src/main/java/com/kajianid/android/activities/MainActivity.java
+++ user/src/main/java/com/kajianid/android/activities/MainActivity.java
@@ -1,16 +1,90 @@
 package com.kajianid.android.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.kajianid.android.R;
+import com.kajianid.android.databinding.ActivityMainBinding;
+
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration mAppBarConfiguration;
+    private boolean doubleBackToExitPressedOnce = false;
+    private DrawerLayout drawerLayout;
+    private ActivityMainBinding binding;
+
+    @Override
+    public void onBackPressed() {
+        if (doubleBackToExitPressedOnce) super.onBackPressed();
+
+        doubleBackToExitPressedOnce = true;
+        Toast.makeText(this, "Tekan sekali lagi untuk keluar dari ShoumHome!", Toast.LENGTH_SHORT).show();
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            doubleBackToExitPressedOnce = false;
+        }, 2000);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        drawerLayout = binding.drawerLayout;
+        setSupportActionBar(binding.appBarMain.toolbar);
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home,
+                R.id.nav_settings,
+                R.id.nav_downloaded_articles,
+                R.id.nav_saved_kajian)
+                .setDrawerLayout(drawerLayout)
+                .build();
+        NavigationUI.setupActionBarWithNavController(this, navController);
+        NavigationUI.setupWithNavController(binding.navMain, navController);
+        View headerView = binding.navMain.getHeaderView(0);
+        TextView txtUsername = headerView.findViewById(R.id.txtNamaUser2);
+        TextView txtEmail = headerView.findViewById(R.id.txtEmail);
+
+        txtUsername.setText(Build.MODEL);
+        txtEmail.setText(Build.MANUFACTURER);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                break;
+            case R.id.menuAbout:
+                Intent i = new Intent(this, AboutActivity.class);
+                startActivity(i);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 }
--- user/src/main/java/com/kajianid/android/activities/SplashActivity.kt
+++ user/src/main/java/com/kajianid/android/activities/SplashActivity.kt
@@ -12,19 +12,9 @@ import com.kajianid.android.databinding.ActivitySplashBinding
 class SplashActivity : AppCompatActivity() {
 
     private var binding: ActivitySplashBinding? = null
-    private var sharedPreferences: SharedPreferences? = null
-
-    private fun setActTheme() {
-        when (sharedPreferences!!.getString("theme", "light")) {
-            "dark" -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-            "light" -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-            "auto" -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
-        }
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setActTheme()
         binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding?.root)
 
