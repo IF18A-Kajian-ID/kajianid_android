53cbfb0507ae668564669844f2f69c643526d22f
fendi-riawan <53111473+fendi-riawan@users.noreply.github.com>
2021-01-15 15:30:30 +0700

    18.11.0268 Fendi Riawan Membuat About
    1. Membuat dan Mengisi Class AboutActivity.java
    2. Membuat dan Mengisi Class AboutPagerAdapter.java
    3. Membuat dan Mengisi Class DevelopersFragment.java
    4. Membuat dan Mengisi Class DevelopersData.java
    5. Membuat dan Mengisi Class activity_about.xml
    6. Membuat dan Mengisi Class fragment_developers.xml

---
+++ admin/src/main/java/com/kajianid/admin/ui/about/AboutActivity.java
@@ -0,0 +1,64 @@
+package com.kajianid.admin.ui.about;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.kajianid.admin.R;
+import com.kajianid.admin.databinding.ActivityAboutBinding;
+
+import java.util.Objects;
+
+public class AboutActivity extends AppCompatActivity {
+
+    private ActivityAboutBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityAboutBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        AboutPagerAdapter adapter = new AboutPagerAdapter(this, getSupportFragmentManager());
+        binding.vpAbout.setAdapter(adapter);
+        binding.tabs.setupWithViewPager(binding.vpAbout);
+
+        setSupportActionBar(binding.toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setTitle(getString(R.string.about));
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_about, menu);
+        return true;
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                super.onBackPressed();
+                break;
+            case R.id.menuGithub:
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse("https://github.com/IF18A-Kajian-ID"));
+                startActivity(i);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        binding = null;
+    }
+}
---
+++ admin/src/main/java/com/kajianid/admin/utils/DeveloperData.java
@@ -0,0 +1,72 @@
+package com.kajianid.admin.utils;
+
+import androidx.annotation.DrawableRes;
+
+import com.kajianid.admin.R;
+
+public class DeveloperData {
+    @DrawableRes
+    public static final int[] drawableId = {
+            R.drawable.dwichan,
+            R.drawable.maratun,
+            R.drawable.fendi,
+            R.drawable.diyah, // Diyah Primasari
+            R.drawable.bayu
+    };
+
+    public static final String[] name = {
+            "Dwi Candra Permana",
+            "Mar Atun Solehah",
+            "Fendi Riawan",
+            "Diyah Primasari",
+            "Muhammad Rizky Bayu Pratama",
+    };
+
+    public static final String[] nim = {
+            "18.11.0004",
+            "18.11.0017",
+            "18.11.0268",
+            "18.11.0289",
+            "18.11.0258",
+    };
+
+    public static final String[] kelas = {
+            "IF 2018 A",
+            "IF 2018 A",
+            "IF 2018 A",
+            "IF 2018 A",
+            "IF 2018 A",
+    };
+
+    public static final String[] yearRegister = {
+            "2018",
+            "2018",
+            "2018",
+            "2018",
+            "2018",
+    };
+
+    public static final String[] githubUsername = {
+            "dwichan0905",
+            "maratun1725",
+            "fendi-riawan",
+            "DiyahPrimasari0289",
+            "rizkybayu1411",
+    };
+
+    public static final String[] email = {
+            "dwichan@outlook.com",
+            "marhatunsoleha17@gmail.com",
+            "fendiriawan0102@gmail.com",
+            "diyahprimasari24@gmail.com",
+            "rizkybayu1411@gmail.com",
+    };
+
+    public static final String[] address = {
+            "Jalan Mandor Martinem Roy 1 Nomor 99 RT 005 RW 006 Desa Rejasari Kecamatan Langensari Kota Banjar",
+            "Desa Losari Rt 04 Rw 04 Kecamatan Rawalo Kabupaten Banyumas",
+            "Kemutug Lor RT 04 RW 03 Kecamatan Baturraden",
+            "Kasih, RT 004 RW 003, Kecamatan Kertanegara, Kabupaten Purbalingga",
+            "Desa Papringan Kec. Banyumas, Kabupaten Banyumas, Jawa Tengah",
+    };
+}
