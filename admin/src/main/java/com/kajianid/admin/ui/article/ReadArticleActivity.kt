08292f038bbeeb313100ceb88c39fbdca7afdac5
maratun1725 <52594711+maratun1725@users.noreply.github.com>
2021-01-15 15:10:10 +0700

    18.11.0017-Mar'Atun Solehah : Membuat class AddUpdateArticleArticleActivity.java, class ReadArticleActivity.kt, class ReadArticleViewModel.java, class Article.java, layout activity_add_update_article.xml
    1.  Membuat class AddUpdateArticleArticleActivity.java
    2. Membuat  class ReadArticleActivity.kt
    3. Membuat  class ReadArticleViewModel.java
    4. Membuat class Article.java
    5. Membuat Layout activity_add_update_article.xml

---
+++ admin/src/main/java/com/kajianid/admin/ui/article/ReadArticleActivity.kt
@@ -0,0 +1,283 @@
+package com.kajianid.admin.ui.article
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ShareCompat
+import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.Glide
+import com.kajianid.admin.R
+import com.kajianid.admin.databinding.ActivityReadArticleBinding
+import com.loopj.android.http.AsyncHttpClient
+import com.loopj.android.http.AsyncHttpResponseHandler
+import cz.msebera.android.httpclient.Header
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import java.util.*
+
+class ReadArticleActivity : AppCompatActivity() {
+    private var binding: ActivityReadArticleBinding? = null
+    private lateinit var readArticleViewModel: ReadArticleViewModel
+    private var id: String? = ""
+    private var title = ""
+    private var content = ""
+    private val isError = false
+
+    companion object {
+        const val EXTRA_ARTICLE_ID = "extra_article_id"
+        const val RESULT_DELETE = 111
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityReadArticleBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+        setSupportActionBar(binding?.toolbar!!)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = getString(R.string.read_article)
+
+        binding?.svArticle?.visibility = View.GONE
+        binding?.progressMessage?.visibility = View.VISIBLE
+        binding?.errorMessage?.visibility = View.GONE
+
+        val bundle = intent.extras
+        if (bundle != null) {
+            id = bundle.getString(EXTRA_ARTICLE_ID)
+            readArticleViewModel = ViewModelProvider(this, ViewModelProvider.NewInstanceFactory())
+                    .get(ReadArticleViewModel::class.java)
+            readArticleViewModel.article.observe(this, {
+                if (it["status"] == true) {
+                    title = it["title"].toString()
+                    content = it["content"].toString()
+                    binding?.contentReadArticle?.tvArticleTitle?.text = it["title"].toString()
+                    if (it["hasImg"] == true) {
+                        Glide.with(this)
+                                .load(
+                                        getString(R.string.server) +
+                                                "assets/articles/" + id + "." +
+                                                Objects.requireNonNull(it["extension"]).toString()
+                                )
+                                .into(binding?.contentReadArticle?.imgArticle!!)
+                    } else {
+                        binding?.contentReadArticle?.imgArticle?.visibility = View.GONE
+                    }
+                    val likes = it["like"].toString()
+                    binding?.contentReadArticle?.tvLikeCount?.text = likes
+                    binding?.contentReadArticle?.tvPostDate?.text = it["post_date"].toString()
+                    binding?.contentReadArticle?.tvPostContent?.text = it["content"].toString()
+                    binding?.progressMessage?.visibility = View.GONE
+                    binding?.errorMessage?.visibility = View.GONE
+                    binding?.svArticle?.visibility = View.VISIBLE
+                } else {
+                    binding?.progressMessage?.visibility = View.GONE
+                    binding?.errorMessage?.visibility = View.VISIBLE
+                    val errorMessage = """
+                    Error!
+                    [${it["code"]}]: ${it["message"]}
+                    """.trimIndent()
+                    binding?.tvErrorMessage?.text = errorMessage
+                }
+            })
+            readArticleViewModel.setArticleAsync(this, id)
+        }
+
+        binding?.pullToRefresh?.setOnRefreshListener {
+            binding?.svArticle?.visibility = View.GONE
+            binding?.pullToRefresh?.isRefreshing = false
+            binding?.errorMessage?.visibility = View.GONE
+            binding?.progressMessage?.visibility = View.VISIBLE
+
+            GlobalScope.launch(Dispatchers.Main) {
+                val deferredArticles = async(Dispatchers.IO) {
+                    id.let { readArticleViewModel.setArticle(applicationContext, it) }
+                }
+                val it = deferredArticles.await()
+                if (it["status"] == true) {
+                    title = it["title"].toString()
+                    content = it["content"].toString()
+                    binding?.contentReadArticle?.tvArticleTitle?.text = it["title"].toString()
+                    if (it["hasImg"] == true) {
+                        Glide.with(applicationContext)
+                                .load(resources.getString(R.string.server) + "assets/articles/${id}.${it["extension"]}")
+                                .into(binding?.contentReadArticle?.imgArticle!!)
+                    } else {
+                        binding?.contentReadArticle?.imgArticle?.visibility = View.GONE
+                    }
+                    val likes = it["like"].toString() + " ${resources.getString(R.string.likes)}"
+                    binding?.contentReadArticle?.tvLikeCount?.text = likes
+                    binding?.contentReadArticle?.tvPostDate?.text = it["post_date"].toString()
+                    binding?.contentReadArticle?.tvPostContent?.text = it["content"].toString()
+                    binding?.progressMessage?.visibility = View.GONE
+                    binding?.svArticle?.visibility = View.VISIBLE
+                } else {
+                    binding?.progressMessage?.visibility = View.GONE
+                    binding?.errorMessage?.visibility = View.VISIBLE
+                    val errorMessage = """
+                            Error!
+                            [${it["code"]}]: ${it["message"]}
+                        """.trimIndent()
+                    binding?.tvErrorMessage?.text = errorMessage
+                }
+            }
+        }
+
+        binding?.btnRefresh?.setOnClickListener { _ ->
+            binding?.svArticle?.visibility = View.GONE
+            binding?.errorMessage?.visibility = View.GONE
+            binding?.progressMessage?.visibility = View.VISIBLE
+            GlobalScope.launch(Dispatchers.Main) {
+                val deferredArticles = async(Dispatchers.IO) {
+                    id.let { readArticleViewModel.setArticle(applicationContext, it) }
+                }
+                val it = deferredArticles.await()
+                if (it["status"] == true) {
+                    title = it["title"].toString()
+                    content = it["content"].toString()
+                    binding?.contentReadArticle?.tvArticleTitle?.text = it["title"].toString()
+                    if (it["hasImg"] == true) {
+                        Glide.with(applicationContext)
+                                .load(resources.getString(R.string.server) + "assets/articles/${id}.${it["extension"]}")
+                                .into(binding?.contentReadArticle?.imgArticle!!)
+                    } else {
+                        binding?.contentReadArticle?.imgArticle?.visibility = View.GONE
+                    }
+                    val likes = it["like"].toString() + " ${resources.getString(R.string.likes)}"
+                    binding?.contentReadArticle?.tvLikeCount?.text = likes
+                    binding?.contentReadArticle?.tvPostDate?.text = it["post_date"].toString()
+                    binding?.contentReadArticle?.tvPostContent?.text = it["content"].toString()
+                    binding?.progressMessage?.visibility = View.GONE
+                    binding?.svArticle?.visibility = View.VISIBLE
+                } else {
+                    binding?.progressMessage?.visibility = View.GONE
+                    binding?.errorMessage?.visibility = View.VISIBLE
+                    val errorMessage = """
+                            Error!
+                            [${it["code"]}]: ${it["message"]}
+                        """.trimIndent()
+                    binding?.tvErrorMessage?.text = errorMessage
+                }
+            }
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> super.onBackPressed()
+            R.id.menuEdit -> {
+                if (isError) {
+                    Toast.makeText(this, resources.getString(R.string.update_error), Toast.LENGTH_SHORT).show()
+                } else {
+                    val i = Intent(this, AddUpdateArticleActivity::class.java)
+                    i.putExtra(AddUpdateArticleActivity.EXTRA_ARTICLE_ID, id)
+                    i.putExtra(AddUpdateArticleActivity.EXTRA_ARTICLE_TITLE, title)
+                    i.putExtra(AddUpdateArticleActivity.EXTRA_ARTICLE_CONTENT, content)
+                    startActivityForResult(i, AddUpdateArticleActivity.RESULT_UPDATE)
+                }
+            }
+            R.id.menuDelete -> {
+                val alert = AlertDialog.Builder(this)
+                alert.setTitle(resources.getString(R.string.sure))
+                        .setMessage(resources.getString(R.string.delete_article_message) + " $title?")
+                        .setPositiveButton(resources.getString(R.string.yes)) { _, _ ->
+                            deleteArticle()
+                        }
+                        .setNegativeButton(resources.getString(R.string.no)) { _, _ ->
+                            /* no-op */
+                        }
+                        .setCancelable(false)
+                val alertDialog = alert.create()
+                alertDialog.show()
+            }
+            R.id.menuShare -> {
+                val mimeType = "text/plain"
+                ShareCompat.IntentBuilder.from(this).apply {
+                    setType(mimeType)
+                    setChooserTitle("Bagikan artikel ini sekarang!")
+                    setText("""
+*${binding?.contentReadArticle?.tvArticleTitle?.text}*
+_${binding?.contentReadArticle?.tvPostDate?.text}_
+
+${binding?.contentReadArticle?.tvPostContent?.text}
+
+Informasi ini disebarkan melalui aplikasi ${getString(R.string.app_name)}.
+                    """.trimIndent())
+                    startChooser()
+                }
+            }
+        }
+        return true
+    }
+
+    private fun deleteArticle() {
+        val url = resources.getString(R.string.server) + "api/articles/$id"
+        val client = AsyncHttpClient()
+        client.delete(url, object : AsyncHttpResponseHandler() {
+            override fun onSuccess(statusCode: Int, headers: Array<out Header>?, responseBody: ByteArray?) {
+                Toast.makeText(this@ReadArticleActivity, resources.getString(R.string.delete_success_message), Toast.LENGTH_SHORT).show()
+                this@ReadArticleActivity.setResult(RESULT_DELETE, Intent())
+                finish()
+            }
+
+            override fun onFailure(statusCode: Int, headers: Array<out Header>?, responseBody: ByteArray?, error: Throwable) {
+                Toast.makeText(this@ReadArticleActivity,
+                        "Oops, an error occured!\n[$statusCode] ${error.message}\n${responseBody.toString()}",
+                        Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_article_kajian, menu)
+        return true
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == RESULT_OK && requestCode == AddUpdateArticleActivity.RESULT_UPDATE) {
+            binding?.svArticle?.visibility = View.GONE
+            binding?.errorMessage?.visibility = View.GONE
+            binding?.progressMessage?.visibility = View.VISIBLE
+            GlobalScope.launch(Dispatchers.Main) {
+                val deferredArticles = async(Dispatchers.IO) {
+                    id.let { readArticleViewModel.setArticle(applicationContext, it) }
+                }
+                val it = deferredArticles.await()
+                if (it["status"] == true) {
+                    title = it["title"].toString()
+                    content = it["content"].toString()
+                    binding?.contentReadArticle?.tvArticleTitle?.text = it["title"].toString()
+                    if (it["hasImg"] == true) {
+                        Glide.with(applicationContext)
+                                .load(resources.getString(R.string.server) + "assets/articles/${id}.${it["extension"]}")
+                                .into(binding?.contentReadArticle?.imgArticle!!)
+                    } else {
+                        binding?.contentReadArticle?.imgArticle?.visibility = View.GONE
+                    }
+                    val likes = it["like"].toString() + " ${resources.getString(R.string.likes)}"
+                    binding?.contentReadArticle?.tvLikeCount?.text = likes
+                    binding?.contentReadArticle?.tvPostDate?.text = it["post_date"].toString()
+                    binding?.contentReadArticle?.tvPostContent?.text = it["content"].toString()
+                    binding?.progressMessage?.visibility = View.GONE
+                    binding?.svArticle?.visibility = View.VISIBLE
+                } else {
+                    binding?.progressMessage?.visibility = View.GONE
+                    binding?.errorMessage?.visibility = View.VISIBLE
+                    val errorMessage = """
+                            Error!
+                            [${it["code"]}]: ${it["message"]}
+                        """.trimIndent()
+                    binding?.tvErrorMessage?.text = errorMessage
+                }
+            }
+        }
+    }
+}
---
+++ admin/src/main/java/com/kajianid/admin/ui/article/ReadArticleViewModel.java
@@ -0,0 +1,169 @@
+package com.kajianid.admin.ui.article;
+
+import android.content.Context;
+import android.os.Looper;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.kajianid.admin.R;
+import com.kajianid.admin.prefs.CredentialPreference;
+import com.kajianid.admin.utils.StringHelper;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+import com.loopj.android.http.SyncHttpClient;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+import cz.msebera.android.httpclient.Header;
+
+public class ReadArticleViewModel extends ViewModel {
+
+    private final MutableLiveData<Map<String, Object>> mData = new MutableLiveData<>();
+
+    public LiveData<Map<String, Object>> getArticle() {
+        return mData;
+    }
+
+    public Map<String, Object> setArticle(Context context, String id) {
+        try {
+            Looper.prepare();
+        } catch (Exception ignored) {
+
+        }
+        Map<String, Object> resultMap = new HashMap<>();
+
+        String url = context.getString(R.string.server) + "api/articles";
+        CredentialPreference credential = new CredentialPreference(context);
+        SyncHttpClient client = new SyncHttpClient();
+
+        RequestParams params = new RequestParams();
+        params.put("read", "1");
+        params.put("ustadz_mode", "1");
+        params.put("ustadz_id", credential.getCredential().getUsername());
+        params.put("article", id);
+
+        client.get(url, params, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                try {
+                    String result = new String(responseBody);
+                    JSONObject resultObject = new JSONObject(result);
+                    JSONArray list = resultObject.getJSONArray("data");
+                    String like = resultObject.getString("likes");
+
+                    JSONObject articleJSONObject = list.getJSONObject(0);
+                    String title = articleJSONObject.getString("title");
+                    boolean hasImg = StringHelper.convertToBoolean(articleJSONObject.getString("has_img"));
+                    String extension = articleJSONObject.getString("extension");
+                    String content = articleJSONObject.getString("content");
+
+                    String date = articleJSONObject.getString("post_date");
+                    Date datePost = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault()).parse(date);
+                    assert datePost != null;
+                    String datePostFormatted = new SimpleDateFormat("dd MMM yyyy HH:mm", Locale.getDefault()).format(datePost);
+
+                    resultMap.put("status", true);
+                    resultMap.put("id", id);
+                    resultMap.put("title", title);
+                    resultMap.put("hasImg", hasImg);
+                    resultMap.put("extension", extension);
+                    resultMap.put("content", content);
+                    resultMap.put("post_date", datePostFormatted);
+                    resultMap.put("like", like);
+                    mData.postValue(resultMap);
+                } catch (JSONException | ParseException e) {
+                    resultMap.put("status", false);
+                    resultMap.put("code", 0);
+                    resultMap.put("message", e.getLocalizedMessage() + " id: " + id);
+                    mData.postValue(resultMap);
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                resultMap.put("status", false);
+                resultMap.put("code", statusCode);
+                resultMap.put("message", error.getLocalizedMessage() + " id: " + id);
+                mData.postValue(resultMap);
+            }
+        });
+
+        return resultMap;
+    }
+
+    public Map<String, Object> setArticleAsync(Context context, String id) {
+        Map<String, Object> resultMap = new HashMap<>();
+
+        String url = context.getString(R.string.server) + "api/articles";
+        CredentialPreference credential = new CredentialPreference(context);
+        AsyncHttpClient client = new AsyncHttpClient();
+
+        RequestParams params = new RequestParams();
+        params.put("read", "1");
+        params.put("ustadz_mode", "1");
+        params.put("ustadz_id", credential.getCredential().getUsername());
+        params.put("article", id);
+
+        client.get(url, params, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                try {
+                    String result = new String(responseBody);
+                    JSONObject resultObject = new JSONObject(result);
+                    JSONArray list = resultObject.getJSONArray("data");
+                    String like = resultObject.getString("likes");
+
+                    JSONObject articleJSONObject = list.getJSONObject(0);
+                    String title = articleJSONObject.getString("title");
+                    boolean hasImg = StringHelper.convertToBoolean(articleJSONObject.getString("has_img"));
+                    String extension = articleJSONObject.getString("extension");
+                    String content = articleJSONObject.getString("content");
+
+                    String date = articleJSONObject.getString("post_date");
+                    Date datePost = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault()).parse(date);
+                    assert datePost != null;
+                    String datePostFormatted = new SimpleDateFormat("dd MMM yyyy HH:mm", Locale.getDefault()).format(datePost);
+
+                    resultMap.put("status", true);
+                    resultMap.put("id", id);
+                    resultMap.put("title", title);
+                    resultMap.put("hasImg", hasImg);
+                    resultMap.put("extension", extension);
+                    resultMap.put("content", content);
+                    resultMap.put("post_date", datePostFormatted);
+                    resultMap.put("like", like);
+                    mData.postValue(resultMap);
+                } catch (JSONException | ParseException e) {
+                    resultMap.put("status", false);
+                    resultMap.put("code", 0);
+                    resultMap.put("message", e.getLocalizedMessage() + " id: " + id);
+                    mData.postValue(resultMap);
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                resultMap.put("status", false);
+                resultMap.put("code", statusCode);
+                resultMap.put("message", error.getLocalizedMessage() + " id: " + id);
+                mData.postValue(resultMap);
+            }
+        });
+
+        return resultMap;
+    }
+}
