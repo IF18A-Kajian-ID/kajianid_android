049a201609067f28f74a5588a7eaa4b02e5aef70
fendi-riawan <53111473+fendi-riawan@users.noreply.github.com>
2021-01-15 10:08:48 +0700

    18.11.0268 Fendi Riawan Membuat Class Kajian dan Admin di Package data
    1. Membuat dan Mengisi Class Kajian.java pada package data
    2. Membuat dan Mengisi Class Admin.java pada package data

---
+++ admin/src/main/java/com/kajianid/admin/data/Admin.java
@@ -0,0 +1,89 @@
+package com.kajianid.admin.data;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Admin implements Parcelable {
+    public static final Creator<Admin> CREATOR = new Creator<Admin>() {
+        @Override
+        public Admin createFromParcel(Parcel in) {
+            return new Admin(in);
+        }
+
+        @Override
+        public Admin[] newArray(int size) {
+            return new Admin[size];
+        }
+    };
+    private String name;
+    private String phone;
+    private String address;
+    private String email;
+    private String gender;
+
+    public Admin() {
+
+    }
+
+    protected Admin(Parcel in) {
+        name = in.readString();
+        phone = in.readString();
+        address = in.readString();
+        email = in.readString();
+        gender = in.readString();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(name);
+        dest.writeString(phone);
+        dest.writeString(address);
+        dest.writeString(email);
+        dest.writeString(gender);
+    }
+}
